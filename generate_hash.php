1198dfbf4046f90349b5253ec32f7b6e46abd44f
Challa Divya <divyach200521@gmail.com>
2025-09-03 23:05:46 +0530

    Removed config/admin files from repo

--- generate_hash.php
+++
@@ -1,6 +0,0 @@
-<?php
-$password = ''; // <<< CHANGE THIS to your desired admin password (e.g., adminpass123)
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-echo "Your desired password (plaintext) is: " . $password . "<br>";
-echo "Your NEW, proper hashed password is: " . $hashed_password;
-?>
